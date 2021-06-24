d70915348bfa6e566d71a0280d1d73e910b86a08
ted <ted.shaughnessy@iress.com>
2021-06-24 15:20:01 +0100

    dfrh

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,5 +3,10 @@ import { GithubService } from './services/github'
 let api = new GithubService('', 'oneiress');
 
 api.repoPrs('caf-marketmaking-widgets').then(x => {
-    console.log(x.length)
+
+    let pr = x[0]
+
+    api.prStatusDetails(pr.base.repo.name, pr.number).then(y => {
+        console.log(y);
+    })
 })
--- api/src/services/github.ts
+++ api/src/services/github.ts
@@ -3,7 +3,7 @@ import { Octokit } from "octokit";
 export class GithubService {
 
     octokit: Octokit
-    owner: string // e.g oneiress or TedShaughnessy
+    owner: string
 
     constructor(token: string, owner: string) {
         this.octokit = new Octokit({
@@ -18,38 +18,81 @@ export class GithubService {
             owner: this.owner,
             repo,
           });
-    
+
           return response.data;
     }
-    
+
     async searchPrs(branchPrefixFilter: string, repoPrefixFilter: string) {
         let head = branchPrefixFilter ? ` head:${branchPrefixFilter}` : '';
-        let org = true ? ` org:${this.owner}` : '';
-    
+
         let response = await this.octokit.rest.search.issuesAndPullRequests({
-            q: `is:open is:pr${head}${org}`
+            q: `is:open is:pr org:${this.owner}${head}`
           })
-    
+
           let prs: any[] = [];
-    
+
           response.data.items.forEach(issue => {
             if (issue.pull_request == null) return;
             if (repoPrefixFilter && !issue.pull_request.html_url.startsWith(`https://github.com/${this.owner}/${repoPrefixFilter}`)) return;
-    
+
             prs.push(issue);
-            issue
           });
-    
+
           return prs[0];
     }
-    
-    async getPr(repo: string, pullRequest: number) {
+
+    async getPr(repo: string, pullRequest: number) { // graphql request is probably better
         let { data } = await this.octokit.rest.pulls.get({
             owner: this.owner,
             repo,
             pull_number: pullRequest
         })
-    
+
         return data
+    }
+
+    async prStatusDetails(repo: string, prNumber: number) { // https://gist.github.com/jwo/e49d06f7f76428fa17fb901e7f418cca
+        let { data } = await this.octokit.graphql(
+`{
+  repository(owner: "${this.owner}", name:"${repo}"){
+    url
+    pullRequest(number: ${prNumber}){
+        number
+        url
+        author {
+          avatarUrl
+          login
+          resourcePath
+          url
+        }
+        commits(last: 1){
+          nodes{
+            commit{
+              commitUrl
+              oid
+              status {
+                state
+                
+                contexts {
+                  state
+                  targetUrl
+                  description
+                  context
+                }
+              }
+            }
+          }
+        }
+    }
+  }
+}
+`,
+            {
+                owner: "octokit",
+                repo: "graphql.js",
+            }
+        );
+
+        return data;
     }
 }
