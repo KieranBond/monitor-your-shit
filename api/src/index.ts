4b9d85fd5380f3697c35a501db44686d3db9b019
Kieran Bond <kieran.bond@iress.com>
2021-06-24 15:24:09 +0100

    finish merge

--- api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { GithubService } from './services/github'
-
-let api = new GithubService('', 'oneiress');
-
-api.repoPrs('caf-marketmaking-widgets').then(x => {
-    console.log(x.length)
-})
