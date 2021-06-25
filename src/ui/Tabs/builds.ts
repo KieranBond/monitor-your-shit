4adba96724c62b1022617e4cd72cd93862a36c0d
Kieran Bond <kieran.bond@iress.com>
2021-06-25 09:49:46 +0100

    add authenticate to bk tab

--- src/ui/Tabs/builds.ts
+++ src/ui/Tabs/builds.ts
@@ -1,6 +1,11 @@
-import {displayData} from "../tab";
+import { displayData } from "../tab";
+import { BuildKiteService } from "../../api/services/buildkite";
+import config from "../../../config.json"
+
+const buildKiteService = new BuildKiteService(config.token.buildkite);
 
 export const openBuildTab = (event: Event) => {
+    buildKiteService.authenticate();
     displayData(event, 'Builds', () => Promise.resolve('<h1>bye</h1>'));
 }
 
